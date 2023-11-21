5bb608a46f5abd5c7727f748b6b693fcaa17b6db
Wessel van Arkel <36850@ma-web.nl>
2023-11-21 15:04:53 +0100

    les1-02

--- 01A/app.js
+++ 01A/app.js
@@ -4,6 +4,21 @@ class App
     {
         console.log("hello world!");
         //code gaat hier!
+        let appNaam = "app"
+        let versienummer = 1.0
+        let versiedatum = new Date("2023-11-21") 
+        let autheur = "Wessel"
+        let copyright = "Yes"
+        let distributeur = "Non"
+        let darkmode = true
+        
+        console.log(appNaam)
+        console.log(versienummer)
+        console.log(versiedatum)
+        console.log(autheur)
+        console.log(copyright)
+        console.log(distributeur)
+        console.log(darkmode)
     }
 }
 
