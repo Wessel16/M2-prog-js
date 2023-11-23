6de7a81d0ea68791722d01e2256278b39f2fc921
Wessel van Arkel <36850@ma-web.nl>
2023-11-23 14:58:30 +0100

    Les 2 02 af

--- 02 canvashuis/app.js
+++ 02 canvashuis/app.js
@@ -2,12 +2,14 @@ class App
 {
     runApplication()
     {
-        console.log("Hello world!");
+        console.log("hello world!")
         let canvas = document.getElementById("canvasId")
         let g = canvas.getContext("2d");
+        g.fillStyle = "purple";
+        g.fillRect(0,0,canvas.width,canvas.height);
+        g.fillStyle = "orange";
         g.fillRect(0,0,10,10);
         console.log(canvas)
-
     }
 }
 
