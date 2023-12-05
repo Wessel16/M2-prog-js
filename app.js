be3859016856c9eb601cba656d8213ead05242d8
Wessel van Arkel <36850@ma-web.nl>
2023-12-05 14:24:41 +0100

    01 van 03 toegevoeg

---
+++ app.js
@@ -0,0 +1,13 @@
+class App
+{
+    runApplication()
+    {
+        console.log("Hello world");
+        //code gaat hier
+        // getElementById
+        // getElementsByClassName
+    }  
+}
+
+let app = new App();
+app.runApplication();
